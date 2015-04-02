750462249880d4d5c5f588c0077426a0774a1a3a
NielsMeulders <niels.meulders@telenet.be>
2015-04-02 15:27:40 +0200

    added bg image

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>MyGuide</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="container">
+        
+        <h1 id="header_splash">WELCOME TO</h1>
+        
+    </div>
+
+</body>
+</html>
